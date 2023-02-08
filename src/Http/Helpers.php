3743cf9c0a3880a15ff95b19f0a85a28a774f2be
Abdessamad MOUDARIR <amoudarir@gmail.com>
2023-02-08 06:12:15 +0100

    Rename CommonHelper.php to Helpers.php
    Rename `CommonHelper.php` to `Helpers.php` and move it to `Http` folder.

--- src/Http/Helpers.php
+++ src/Http/Helpers.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Moudarir\CodeigniterApi\Helpers;
+namespace Moudarir\CodeigniterApi\Http;
 
-class CommonHelper
+class Helpers
 {
 
     /**
@@ -73,5 +73,85 @@ class CommonHelper
         }
 
         return lcfirst(str_replace(' ', '', ucwords(str_replace($separator, ' ', $string))));
+    }
+
+    /**
+     * @param array|string|null $with
+     * @return array|null
+     */
+    public static function formatApiWith($with = null): ?array
+    {
+        if ($with === null) {
+            return null;
+        }
+
+        if (!is_array($with)) {
+            $with = [$with];
+        }
+
+        $data = [];
+
+        foreach ($with as $key => $value) {
+            if (!is_string($key)) {
+                $data[$value] = true;
+            } else {
+                $data[$key] = $value;
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param string $string
+     * @param int $start
+     * @param int|null $length
+     * @return string|null
+     */
+    public static function charsFromPosition(string $string, int $start = 0, ?int $length = null): ?string
+    {
+        $char = substr($string, $start, $length);
+
+        return $char !== '' ? $char : null;
+    }
+
+    /**
+     * @param string $word
+     * @param string|null $format
+     * @return string|null
+     */
+    public static function firstLetter(string $word, ?string $format = null): ?string
+    {
+        $letter = self::charsFromPosition($word, 0, 1);
+        switch ($format) {
+            case 'upper':
+            default:
+                $letter = strtoupper($letter);
+                break;
+            case 'lower':
+                $letter = strtolower($letter);
+                break;
+        }
+
+        return $letter;
+    }
+
+    /**
+     * @param string $string
+     * @param string $separator
+     * @param string|null $format
+     * @return string
+     */
+    public static function firstLetters(string $string, string $separator = '.', ?string $format = null): string
+    {
+        $keywords = explode(' ', trim($string));
+        $letters = [];
+        foreach ($keywords as $word) {
+            if (trim($word) !== '') {
+                $letters[] = self::firstLetter($word, $format);
+            }
+        }
+
+        return implode($separator, $letters);
     }
 }

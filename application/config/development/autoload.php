73235effa7f8dbf8a1b53e5465e9840d8eb135d2
Abdessamad MOUDARIR <amoudarir@gmail.com>
2023-02-08 19:23:20 +0100

    Remove unused folders and files

--- application/config/development/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$autoload['packages'] = [];
-$autoload['libraries'] = [];
-$autoload['drivers'] = [];
-$autoload['helper'] = ['url'];
-$autoload['config'] = [];
-$autoload['language'] = [];
-$autoload['model'] = [];

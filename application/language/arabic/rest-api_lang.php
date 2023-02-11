93923d7b38e7308802852d1e5f7671462d315ced
Abdessamad MOUDARIR <amoudarir@gmail.com>
2023-02-11 02:54:07 +0100

    Add Arabic language support
    ... and adding new error messages.

---
+++ application/language/arabic/rest-api_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['rest_https_protocol_required'] = "بروتوكول HTTPS مطلوب";
+$lang['rest_unknown_path'] = "مسار غير معروف";
+$lang['rest_allow_ajax_only'] = "يسمح فقط بالطلبات الواردة من AJAX";
+$lang['rest_ip_whitelist'] = "عنوان IP غير مسموح به";
+$lang['rest_ip_blacklist'] = "عنوان IP مرفوض";
+$lang['rest_not_found'] = "غير موجود";
+$lang['rest_api_key_not_found'] = "مفتاح API مفقود";
+$lang['rest_api_ip_address_denied'] = "عنوان IP غير مسموح به";
+$lang['rest_empty_username_or_password'] = "خطأ في الإتصال";
+$lang['rest_auth_key_not_found'] = "مفتاح الرمز غير موجود";
+$lang['rest_jwt_expired_token'] = "انتهت صلاحية مفتاح الرمز المميز";
+$lang['rest_jwt_auth_failed'] = "Bearer authentication failed.";
+$lang['rest_jwt_internal_server_error'] = "Internal server error.";
+$lang['rest_jwt_signature_verification_failed'] = "Bearer token signature verification failed.";
+$lang['rest_unauthorized'] = "Unauthorized.";
+$lang['rest_forbidden_access'] = "Forbidden access.";
+$lang['rest_method_not_allowed'] = "Method not found.";
+$lang['rest_response_format_not_supported'] = 'The "%s" response format is not supported.';
+$lang['rest_invalid_jsonp_callback'] = "INVALID JSONP CALLBACK: %s";

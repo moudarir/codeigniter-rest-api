faab654b52a64b93b068b991e91bf893819688fc
Abdessamad MOUDARIR <amoudarir@gmail.com>
2023-02-11 05:47:18 +0100

    Remove unused tables

--- dumping/queries.sql
+++ dumping/queries.sql
@@ -1,19 +1,5 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-CREATE TABLE `roles` (
-  `id` tinyint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
-
-INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'moderator', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
-(2, 'admin', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
-(3, 'super', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
-(4, 'member', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-
 CREATE TABLE `users` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `firstname` varchar(50) NOT NULL,
@@ -29,23 +15,6 @@ CREATE TABLE `users` (
 # password: Hwkh3LEdsJWhEF3
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `created_at`, `updated_at`) VALUES
 (1, 'John', 'DOE', 'john@doe.com', '$argon2i$v=19$m=16384,t=4,p=2$bzJMdUQ3dm4uUi43SDJGWg$MQQtuNV44p5l8SwxxLRiNLPml2IAt3zbirXYMQiziN0', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-
-CREATE TABLE `users_roles` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_id` int UNSIGNED NOT NULL,
-  `role_id` tinyint UNSIGNED NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ug_users_roles` (`user_id`,`role_id`),
-  KEY `user_id` (`user_id`),
-  KEY `role_id` (`role_id`),
-  CONSTRAINT `users_roles_roles_fk1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `users_roles_users_fk1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB;
-
-INSERT INTO `users_roles` (`id`, `user_id`, `role_id`, `created_at`, `updated_at`) VALUES
-(1, 1, 3, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 CREATE TABLE `api_keys`(
   `id` int unsigned NOT NULL AUTO_INCREMENT,
